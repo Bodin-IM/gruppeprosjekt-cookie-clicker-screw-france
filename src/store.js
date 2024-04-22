a1558f9d7a9491a556d43e7366ba2c11dfbad2bf
KapteinB <143996495+KapteinB@users.noreply.github.com>
2024-04-22 14:30:16 +0200

    Jerald part 2

--- src/store.js
+++ src/store.js
@@ -5,20 +5,35 @@ import { getInventory } from "./user.js";
  * Aquí puede cambiar el precio de los artículos -
  * - este sistema es malo porque si 2 artículos tienen el mismo precio, el sistema se romperá (Yo creo)
 */
+/*
+* Se corrigieron algunos de los precios debido a cómo funciona la escala (la actualización aumenta el precio cada vez que se compran)
+* - y agregué algunas de las otras actualizaciones que aún no se muestran en este script.
+* - Quería que los precios de las actualizaciones aumentaran entre un 10% y un 50% cada vez que se compraran.
+*/
 const sItems = {
     "Mouse": 1,
     "Auto click": 10,
-    "Homeless guy": 25,
-    "Case": 75,
-    "Motherboard": 200,
-    "RAM": 500,
-    "Cpu": 1000,
-    "Cpu cooler": 2000,
-    "Graphics card": 5000,
-    "Psu": 10000,
-    "Complete pc": 20000,
-    "Bird thing": 50000,
-    "Satelite": 100000
+    "Homeless guy": 50,
+    "Case": 250,
+    "Motherboard": 550,
+    "RAM": 1500,
+    "Cpu": 4000,
+    "Cpu cooler": 12500,
+    "Graphics card": 50000,
+    "Psu": 140000,
+    "Complete pc": 210000,
+    "Bird thing": 350000,
+    "Satelite": 500000,
+    "Rover": 1000000,
+    "Dyson Sphere": 4200000,
+    "Black Hole Generator": 8800000,
+    "Time Machine": 3210000,
+    "Dimentional Gate": 100000000,
+    "Parallel Universes": 500000000,
+    "VOID": 1000000000,
+    "Jerald": 6666666666,
+
+
 };
 
 function buy(item) {
